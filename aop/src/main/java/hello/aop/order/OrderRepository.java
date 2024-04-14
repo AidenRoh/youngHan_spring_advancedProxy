c7d84d341266979437a3a1e5f3c1d7ae8a795fd6
AidenRoh <colohrby@gmail.com>
2024-04-14 22:30:36 +0900

    feature: add simple aop class

--- aop/src/main/java/hello/aop/order/OrderRepository.java
+++ aop/src/main/java/hello/aop/order/OrderRepository.java
@@ -7,13 +7,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class OrderRepository {
 
-    public String save(String itemId) {
-        log.info("[orderRepository] 실행");
-        //저장 로직
-        if (itemId.equals("ex")) {
-            throw new IllegalStateException("예외 발생!");
-        }
-        return "ok";
-    }
-
+	public String save(String itemId) {
+		log.info("[orderRepository] 실행");
+		// save logic
+		if (itemId.equals("ex")) {
+			throw new IllegalStateException("예외 발생!");
+		}
+		return "ok";
+	}
 }
--- aop/src/main/java/hello/aop/order/OrderService.java
+++ aop/src/main/java/hello/aop/order/OrderService.java
@@ -7,15 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderService {
 
-    private final OrderRepository orderRepository;
+	private final OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }
+	public OrderService(OrderRepository orderRepository) {
+		this.orderRepository = orderRepository;
+	}
 
-    public void orderItem(String itemId) {
-        log.info("[orderService] 실행");
-        orderRepository.save(itemId);
-    }
-
+	public void orderItem(String itemId) {
+		log.info("[orderService] 실행");
+		orderRepository.save(itemId);
+	}
 }
