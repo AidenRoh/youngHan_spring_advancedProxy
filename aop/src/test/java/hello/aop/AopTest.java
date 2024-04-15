47d2b0ba7e38cd54916c797bb297b7af1213754f
AidenRoh <colohrby@gmail.com>
2024-04-15 22:35:25 +0900

    feature: add aop annotation

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,15 +2,18 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV5Order;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopTest {
 
 	@Autowired
@@ -21,8 +24,8 @@ public class AopTest {
 
 	@Test
 	void aopInfo() {
-		log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
-		log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+		System.out.println("isAopProxy, orderService=" + AopUtils.isAopProxy(orderService));
+		System.out.println("isAopProxy, orderRepository=" + AopUtils.isAopProxy(orderRepository));
 	}
 
 	@Test
@@ -32,7 +35,6 @@ public class AopTest {
 
 	@Test
 	void exception() {
-		orderService.orderItem("ex");
 		Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
 				.isInstanceOf(IllegalStateException.class);
 	}
